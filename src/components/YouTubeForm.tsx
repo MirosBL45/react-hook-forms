bc7cf494aeabdb38a91e9e7baceba9b9adabe29a
MirosBL45 <jovicmiroslav88@gmail.com>
2024-11-22 14:33:49 +0100

    added devtool

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,20 +1,27 @@
+import { useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+
 export default function YouTubeForm() {
+  const formDeal = useForm();
+  const { register, control } = formDeal;
+
   return (
     <div>
       <h1>YouTube Form</h1>
 
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register('username')} />
 
         <label htmlFor="email">E-mail</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register('email')} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register('channel')} />
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 }
